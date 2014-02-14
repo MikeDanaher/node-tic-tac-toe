a2c58bc326603a0ef4484ad3649ea05f1c14b4e0
Mike Danaher <danaher.mike@gmail.com>
2014-02-14 16:20:45 -0600

    Refactored check for winner to include a function to tell who the winner is

--- src/checkForWinner.js
+++ src/checkForWinner.js
@@ -1,18 +1,31 @@
 var Winner = {};
 
-Winner.check = function(winsArray) {
-    var isWinner = false;
+Winner.who = function(winsArray) {
+    var winningPlayer;
 
     for (var i = 0; i < winsArray.length; i++) {
-        var test = winsArray[i].join('');
-        if (test === 'xxx' || test === 'ooo') {
-            isWinner = true;
+        var row = winsArray[i];
+        if (row[0] === 'x' && row[1] === 'x' && row[2] === 'x') {
+            winningPlayer = 'x';
+            break;
+        } else if (row[0] === 'o' && row[1] === 'o' && row[2] === 'o') {
+            winningPlayer = 'o';
             break;
         }
     }
 
-    return isWinner;
+    return winningPlayer;
 
+};
+
+Winner.check = function(winsArray) {
+    var winningPlayer = Winner.who(winsArray);
+
+    if (winningPlayer) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 module.exports = Winner;
